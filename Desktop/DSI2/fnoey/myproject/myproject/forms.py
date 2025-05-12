36214111939a47139322385290d6f943f9df8042
Kantima-Trichaiya <kantimatrichaiya2547@gmail.com>
2025-05-12 20:20:04 +0700

    updatee

--- Desktop/DSI2/fnoey/myproject/myproject/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Rental
-
-class RentalForm(forms.ModelForm):
-    class Meta:
-        model = Rental
-        fields = []  # No fields needed; user, equipment, and start_time are set programmatically
